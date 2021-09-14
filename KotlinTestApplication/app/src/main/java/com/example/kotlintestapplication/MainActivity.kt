88b8e218dd0099c78f23a452a4d0342f7f2f12c8
ssano <kssho3364@gmail.com>
2021-09-14 17:03:34 +0900

    210914_KSH
    practice Kotlin

--- KotlinTestApplication/app/src/main/java/com/example/kotlintestapplication/MainActivity.kt
+++ KotlinTestApplication/app/src/main/java/com/example/kotlintestapplication/MainActivity.kt
@@ -1,22 +1,42 @@
 package com.example.kotlintestapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kotlintestapplication.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var mBinding : ActivityMainBinding? =null
-    private val binding get() = mBinding!!
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        database = FirebaseDatabase.getInstance().reference
+
         binding.textview.setOnClickListener {
-            binding.textview.setText("Hello, Kotlin")
+//            val now = System.currentTimeMillis()
+//            val date = Date(now)
+//            val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm")
+//            val getTime = dateFormat.format(date)
+
+
+            val name = database.child("Company").key
+            binding.textview.setText(name)
+        }
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
